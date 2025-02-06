d87e6012beb4418522b2bada29fe622e714485ec
shafeeh011 <shafeeh011@.com>
2025-02-06 13:15:38 +0530

    data validation

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.mlProject.pipeline.stage_02_data_validtion import DataValiadtionTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValiadtionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlProject/pipeline/stage_02_data_validtion.py
+++ src/mlProject/pipeline/stage_02_data_validtion.py
@@ -1,26 +1,28 @@
 from src.mlProject.config.configuration import ConfigurationManager
-from src.mlProject.components.data_ingestion import DataIngestion
 from src.mlProject import logger
+from src.mlProject.components.data_validation import DataValiadtion
 
 STAGE_NAME = "Data Ingestion stage"
 
-class DataIngestionTrainingPipeline:
+
+class DataValiadtionTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        try:
+            config = ConfigurationManager()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValiadtion(config=data_validation_config)
+            data_validation.validate_all_columns()
+        except Exception as e:
+            raise e
         
-        
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = DataValiadtionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
